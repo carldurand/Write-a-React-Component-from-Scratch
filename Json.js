6163b3e10b69802b6f45d1fb7ad2469f263e8e75
Carl Durand <carldurand.student@careerdevs.com>
2019-06-13 17:19:24 +0000

    Convert JSON Data to HTML

--- Json.js
+++ Json.js
@@ -1,16 +1,26 @@
 <script>
   document.addEventListener('DOMContentLoaded',function(){
     document.getElementById('getMessage').onclick=function(){
-      // Add your code below this line
       req=new XMLHttpRequest();
-req.open("GET",'/json/cats.json',true);
-req.send();
-req.onload=function(){
-  json=JSON.parse(req.responseText);
-  document.getElementsByClassName('message')[0].innerHTML=JSON.stringify(json);
-};
-      
-      // Add your code above this line
+      req.open("GET",'/json/cats.json',true);
+      req.send();
+      req.onload=function(){
+        json=JSON.parse(req.responseText);
+        var html = "";
+        // Add your code below this line
+        json.forEach(function(val) {
+  var keys = Object.keys(val);
+  html += "<div class = 'cat'>";
+  keys.forEach(function(key) {
+    html += "<strong>" + key + "</strong>: " + val[key] + "<br>";
+  });
+  html += "</div><br>";
+});
+        
+        
+        // Add your code above this line
+        document.getElementsByClassName('message')[0].innerHTML=html;
+      };
     };
   });
 </script>
