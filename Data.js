71deb0ad170057642464b476f8537ac0d5c7d1d3
Carl Durand <carldurand.student@careerdevs.com>
2019-06-16 00:36:23 +0000

    Create a Bar for Each Data Point in the Set

--- Data.js
+++ Data.js
@@ -1,11 +1,27 @@
 <body>
   <script>
-    // Add your code below this line
-    d3.select("body")
-      .append("h1")
-      .text("Learning D3");
+    const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
     
+    const w = 500;
+    const h = 100;
     
-    // Add your code above this line
+    const svg = d3.select("body")
+                  .append("svg")
+                  .attr("width", w)
+                  .attr("height", h);
+    
+    svg.selectAll("rect")
+       // Add your code below this line
+       d3.select("body").selectAll("div")
+         .data(dataset)
+         .enter()
+         .append("rect")
+       
+       
+       // Add your code above this line
+       .attr("x", 0)
+       .attr("y", 0)
+       .attr("width", 25)
+       .attr("height", 100);
   </script>
 </body>
