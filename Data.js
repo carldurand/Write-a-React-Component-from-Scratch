560cf2d7bc284c85d4e8ea792bb97ab95b3dab6e
Carl Durand <carldurand.student@careerdevs.com>
2019-06-17 15:25:26 +0000

    seek and destroy algorithm

--- Data.js
+++ Data.js
@@ -1,37 +1,12 @@
-<body>
-  <script>
-    const dataset = [
-                  [ 34,    78 ],
-                  [ 109,   280 ],
-                  [ 310,   120 ],
-                  [ 79,    411 ],
-                  [ 420,   220 ],
-                  [ 233,   145 ],
-                  [ 333,   96 ],
-                  [ 222,   333 ],
-                  [ 78,    320 ],
-                  [ 21,    123 ]
-                ];
-    
-    
-    const w = 500;
-    const h = 500;
-    
-    const svg = d3.select("body")
-                  .append("svg")
-                  .attr("width", w)
-                  .attr("height", h);
-    
-    svg.selectAll("circle")
-       .data(dataset)
-       .enter()
-       .append("circle")
-       // Add your code below this line
-       .attr("cx", (d, i) => d[0])
-       .attr("cy", (d, i) => h - d[1])
-       .attr("r", 5);
-       
-       // Add your code above this line
-  
-  </script>
-</body>
+function destroyer(arr) {
+  var args = Array.prototype.slice.call(arguments);
+
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < args.length; j++) {
+      if (arr[i] === args[j]) {
+        delete arr[i];
+      }
+    }
+  }
+  return arr.filter(Boolean);
+}
